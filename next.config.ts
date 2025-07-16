a3293e1b6f1af02e1c55be1e4fdba5d63ca5f3cf
ahussain <Armaash-Hussain@Armaash-Hussain.local>
2025-07-16 21:58:49 +0500

    chore(eslint): disabled linting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+/* eslint-disable */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
 };
 
 export default nextConfig;
